c890c5f2a120bd23ca4486a8640cfacd55df1248
william-spec <52228900+william-spec@users.noreply.github.com>
2021-10-11 10:56:34 +0800

    Update test.js

--- test.js
+++ test.js
@@ -50,3 +50,33 @@ function findKthLargest(nums, n) {       //找到第K大的数
 
 let n = [3,2,3,1,2,4,5,5,6];
 findKthLargest(n, 4);
+
+
+var smallestK = function(arr, k) {      //找到最小的K个数
+    let res = [];
+    quickSort(arr, k, 0, arr.length - 1, res);
+    return res;
+};
+function quickSort(arr, k, l, r, res){
+    if(l>=r)return;
+    let index = getIndex(arr, l, r);
+    if(index > k - 1)
+        quickSort(arr,k,l, index - 1, res);
+    else if(index < k - 1)
+        quickSort(arr,k,index+1, r, res);
+    else
+        res.push(...arr.slice(0, k));
+}
+function getIndex(arr, l, r){
+    let temp = arr[l];
+    while(l<r){
+        while(l<r && arr[r]>=temp)r--;
+        arr[l]=arr[r];
+        while(l<r && arr[l]<temp)l++;
+        arr[r]=arr[l];
+    }
+    arr[l] = temp;
+    return l;
+}
+let a = [1,3,5,7,2,4,6,8];
+console.log(smallestK(a, 4));
