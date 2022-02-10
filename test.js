a657b96833ce7e585e3af5a3848f10cb69e0e03d
william-spec <52228900+william-spec@users.noreply.github.com>
2022-02-10 14:45:47 +0800

    Update test.js

--- test.js
+++ test.js
@@ -33,7 +33,6 @@ Function.prototype.myApply = function(o){
 Function.prototype.myBind = function(o){
   let that = this;
   let arguments1 = [].slice.call(arguments, 1);
-  let emptyf = function(){};
   let newf = function(){
     let arguments2 = [].slice.call(arguments);
     if(this instanceof newf)    //如果使用new调用newf，那么this指向新创建的实例，且在同一条原型链上，条件为true
@@ -41,8 +40,10 @@ Function.prototype.myBind = function(o){
     else
       return that.apply(o, arguments1.concat(arguments2));
   }
+  let emptyf = function(){};
   emptyf.prototype = that.prototype;    //以空函数作为跳板，将空函数的原型指向构造函数的原型
   newf.prototype = new emptyf();    //将newf函数也就是使用new关键字时的构造函数的原型指向emptyf实例，实现原型链继承
+  //newf.prototype = new that();    也可以实现原型链继承
   return newf;
 }
 
